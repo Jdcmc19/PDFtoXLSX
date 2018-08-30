669e6964875e4273c038cf17d66e31a5f5b3230c
Jdcmc19 <35380586+Jdcmc19@users.noreply.github.com>
2018-08-30 10:28:57 -0600

    Final

--- src/Classes/Excel.java
+++ src/Classes/Excel.java
@@ -1,6 +1,8 @@
 package Classes;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileOutputStream;
@@ -10,8 +12,10 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Excel {
-    public static void escribirExcel(String path,ArrayList<Transaccion> trans) throws IOException,Exception{
+    public static void escribirExcel(String path,ArrayList<Transaccion> trans, Informacion informacion) throws IOException,Exception{
         String[] columns = {"FECHA", "TIPO DOCUMENTO", "DESCRIPCIÓN","CREDITO/FACTURA","DEBITO/PAGO","SALDO"};
+
+
 
         Workbook workbook = new XSSFWorkbook();
         CreationHelper createHelper = workbook.getCreationHelper();
@@ -21,24 +25,85 @@ public class Excel {
         headerFont.setFontHeightInPoints((short) 12);
         headerFont.setColor(IndexedColors.BLACK.getIndex());
 
+        CellStyle normal = workbook.createCellStyle();
+        normal.setBorderBottom(BorderStyle.THIN);
+        normal.setBorderTop(BorderStyle.THIN);
+        normal.setBorderRight(BorderStyle.THIN);
+        normal.setBorderLeft(BorderStyle.THIN);
+
         CellStyle headerCellStyle = workbook.createCellStyle();
         headerCellStyle.setFont(headerFont);
+        headerCellStyle.setBorderBottom(BorderStyle.THIN);
+        headerCellStyle.setBorderTop(BorderStyle.THIN);
+        headerCellStyle.setBorderRight(BorderStyle.THIN);
+        headerCellStyle.setBorderLeft(BorderStyle.THIN);
 
         CellStyle dateStyle = workbook.createCellStyle();
         dateStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd/MM/yyyy"));
+        dateStyle.setBorderBottom(BorderStyle.THIN);
+        dateStyle.setBorderTop(BorderStyle.THIN);
+        dateStyle.setBorderRight(BorderStyle.THIN);
+        dateStyle.setBorderLeft(BorderStyle.THIN);
 
         CellStyle numbStyle = workbook.createCellStyle();
         numbStyle.setDataFormat(createHelper.createDataFormat().getFormat("###,###,###.#0"));
+        numbStyle.setBorderBottom(BorderStyle.THIN);
+        numbStyle.setBorderTop(BorderStyle.THIN);
+        numbStyle.setBorderRight(BorderStyle.THIN);
+        numbStyle.setBorderLeft(BorderStyle.THIN);
 
-        Row headerRow = sheet.createRow(0);
+        Row titulo = sheet.createRow(0);
+        Cell celda = titulo.createCell(0);
+        celda.setCellStyle(headerCellStyle);
+        celda.setCellValue("ESTADO DE CUENTA");
+
+        Row title = sheet.createRow(2);
+
+        String[] infor = {"CLAVE CLIENTE", "NOMBRE", "MONEDA","FECHA DE CORTE"};
+        String[] inforRes = {informacion.getClaveCliente(),informacion.getNombre(),informacion.getMoneda()};
+
+        for(int i = 0; i < infor.length; i++){
+            Cell cell = title.createCell(i);
+            cell.setCellValue(infor[i]);
+            cell.setCellStyle(headerCellStyle);
+        }
+
+        Row titleResponses = sheet.createRow(3);
+
+        for(int i = 0; i < inforRes.length; i++){
+            Cell cell = titleResponses.createCell(i);
+            cell.setCellValue(inforRes[i]);
+            cell.setCellStyle(normal);
+        }
+
+        Cell cell = titleResponses.createCell(inforRes.length);
+        cell.setCellStyle(dateStyle);
+        cell.setCellValue(informacion.getFechaCorte());
+
+        String[] infor2 = {"SALDO TOTAL", "SALDO CRÉDITO REVOLUTIVO", "SALDO DERECHO DE MARCA"};
+        Double[] infor2res = {informacion.getSaldoTotal(), informacion.getSaldoCreditoRevolutivo(), informacion.getDerechoMarca()};
+
+        Row title2 = sheet.createRow(4);
+        Row title2res = sheet.createRow(5);
+
+        for(int i = 0; i < infor2.length; i++){
+            cell = title2.createCell(i);
+            Cell cell2 = title2res.createCell(i);
+            cell2.setCellValue(infor2res[i]);
+            cell2.setCellStyle(numbStyle);
+            cell.setCellValue(infor2[i]);
+            cell.setCellStyle(headerCellStyle);
+        }
+
+        Row headerRow = sheet.createRow(7);
 
         for(int i = 0; i < columns.length; i++) {
-            Cell cell = headerRow.createCell(i);
+            cell = headerRow.createCell(i);
             cell.setCellValue(columns[i]);
             cell.setCellStyle(headerCellStyle);
         }
 
-        int rowNum = 1;
+        int rowNum =8;
         for(Transaccion transac: trans) {
            // System.out.println(transac.toString());
             Row row = sheet.createRow(rowNum++);
@@ -50,9 +115,12 @@ public class Excel {
 
             row.createCell(1)
                     .setCellValue(transac.getTipoDocumento());
+            row.getCell(1).setCellStyle(normal);
 
             row.createCell(2)
                     .setCellValue(transac.getDescripcion());
+            row.getCell(2).setCellStyle(normal);
+
             if(transac.getTipoDocumento().equals("FACTURA")){
                 row.createCell(3)
                         .setCellValue(transac.getCredito());
@@ -67,9 +135,52 @@ public class Excel {
                     .setCellValue(transac.getSaldo());
             row.getCell(5).setCellStyle(numbStyle);
         }
+
+        Row row = sheet.createRow(rowNum++);
+        row.createCell(4).setCellValue("SALDO");
+        row.getCell(4).setCellStyle(headerCellStyle);
+        row.createCell(5).setCellStyle(numbStyle);
+        row.getCell(5).setCellValue(informacion.getSaldo());
+
+        rowNum++;
+
+        row = sheet.createRow(rowNum++);
+        row.createCell(0).setCellValue("CRÉD REV");
+        row.getCell(0).setCellStyle(headerCellStyle);
+        row.createCell(1).setCellValue(informacion.getSaldoCreditoRevolutivo2());
+        row.getCell(1).setCellStyle(numbStyle);
+
+        row = sheet.createRow(rowNum++);
+        row.createCell(0).setCellValue("DERECHO DE MARCA");
+        row.getCell(0).setCellStyle(headerCellStyle);
+        row.createCell(1).setCellStyle(numbStyle);
+        row.getCell(1).setCellValue(informacion.getDerechoMarca2());
+
+        rowNum++;
+
+        row = sheet.createRow(rowNum++);
+        row.createCell(0).setCellValue("TOTAL");
+        row.getCell(0).setCellStyle(headerCellStyle);
+        row.createCell(1).setCellStyle(numbStyle);
+        row.getCell(1).setCellValue(informacion.getTotal());
+
+
+        row = sheet.createRow(rowNum++);
+        row.createCell(0).setCellValue("DISPONIBLE CRÉDITO REV");
+        row.getCell(0).setCellStyle(headerCellStyle);
+        row.createCell(1).setCellStyle(numbStyle);
+        row.getCell(1).setCellValue(informacion.getDisponibleCreditoRevolutivo());
+
         for(int i = 0; i < columns.length; i++) {
             sheet.autoSizeColumn(i);
         }
+
+        rowNum++;
+        row = sheet.createRow(rowNum++);
+        row.createCell(0).setCellValue(informacion.getMensaje());
+        row.getCell(0).setCellStyle(normal);
+
+
         FileOutputStream fileOutputStream = new FileOutputStream(path);
         workbook.write(fileOutputStream);
         fileOutputStream.close();
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,216 @@
 package sample;
 
+import Classes.Excel;
+import Classes.Informacion;
+import Classes.PDFManager;
+import Classes.Transaccion;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Pane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Controller {
+    @FXML
+    Button btoListo, btoSetDirectory, btoGetFile;
+
+    @FXML
+    TextField txtPDF, txtExc, txtName;
+
+    @FXML
+    Pane pane;
+
+    public void initialize(){
+
+        btoListo.setOnAction(event -> {
+            System.out.println("hola");
+            btoListo.setDisable(true);
+            if(!txtExc.getText().equals("") && !txtPDF.getText().equals("") && !txtName.getText().equals("")){
+                try{
+                    getTransacciones(txtPDF.getText(),txtExc.getText()+"\\"+txtName.getText()+".xlsx");
+                }catch (Exception e ){
+                    e.printStackTrace();
+                }
+            }else
+            {
+                Platform.runLater(
+                        () -> {
+                            Alert alert = new Alert(Alert.AlertType.ERROR);
+                            alert.setTitle("Error.");
+                            alert.setHeaderText("Falta de información.");
+                            alert.setContentText("Rellene la información necesaria antes de convertir.");
+                            alert.showAndWait().ifPresent(rs -> {
+                                if (rs == ButtonType.OK) {
+                                    alert.close();
+                                }
+                            });});
+            }
+            btoListo.setDisable(false);
+        });
+        btoGetFile.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            File file = fileChooser.showOpenDialog(null);
+            if(file!=null)txtPDF.setText(file.getAbsolutePath());
+        });
+        pane.setOnDragOver(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(DragEvent event) {
+                Dragboard db = event.getDragboard();
+                if (db.hasFiles()) {
+                    event.acceptTransferModes(TransferMode.COPY);
+                } else {
+                    event.consume();
+                }
+            }
+        });
+        pane.setOnDragDropped(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(DragEvent event) {
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasFiles()) {
+                    success = true;
+                    String filePath = null;
+                    for (File file:db.getFiles()) {
+                        filePath = file.getAbsolutePath();
+                        txtPDF.setText(filePath);
+                    }
+                }
+                event.setDropCompleted(success);
+                event.consume();
+            }
+        });
+
+        btoSetDirectory.setOnAction(event -> {
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            File selectedDirectory = directoryChooser.showDialog(null);
+            txtExc.setText(selectedDirectory.getAbsolutePath());
+        });
+    }
+
+    public void getTransacciones(String pathPDF, String pathExc) throws Exception {
+        PDFManager pdfManager = new PDFManager();
+        pdfManager.setFilePath(pathPDF);
+        try{
+            String[] fk = pdfManager.ToText().split("\n");
+            ArrayList<Transaccion> transaccions = new ArrayList<>();
+            int cont = 0;
+            String saldoTotal = "";
+            Informacion info = new Informacion();
+            String nombre = "";
+            Boolean banderaSaldo = false;
+            Boolean banderaClave = false;
+            Boolean banderaNombre = false;
+            Boolean banderaMoneda = false;
+            for(String a : fk){
+                a=a.replaceAll("\\s+",";");
+                String[] fkfk = a.split(";");
+                if(fkfk.length>2 && (fkfk[1].equals("FACTURA") || fkfk[1].equals("NOTA") || fkfk[1].equals("PAGO"))) {
+                    //ystem.out.println(a);
+                    String descr = "";
+                    for (int i = 0; i < fkfk.length - 4; i++) {
+                        descr += (fkfk[i + 2] + " ");
+                    }
+                    transaccions.add(new Transaccion(fkfk[0], fkfk[1], descr, fkfk[fkfk.length - 2], fkfk[fkfk.length - 1]));
+                }
+                else if(cont==2){
+                    info.setSaldoTotal(a);
+                    System.out.println("se mando este: "+a+" y llego esto: "+info.getSaldoTotal());
+                }
+                else if(cont>3){
+                    if(fkfk[0].equals("SALDO") && !fkfk[1].equals("crédito")){
+                        info.setSaldo(fkfk[1]);
+                    }else if(fkfk[0].equals("CRÉDITO")){
+                        info.setSaldoCreditoRevolutivo(fkfk[2]);
+                    }else if(fkfk[0].equals("DERECHO")){
+                        info.setDerechoMarca(fkfk[3]);
+                    }else if(fkfk[0].equals("TOTAL")){
+                        info.setTotal(fkfk[1]);
+                    }else if(fkfk[0].equals("DISPONIBLE")){
+                        info.setDisponibleCreditoRevolutivo(fkfk[3]);
+                    }else if(fkfk[0].equals("Estimado")){
+                        info.setMensaje(a.replace(";"," "));
+                    }else if(fkfk[0].equals("Saldo")&& fkfk[1].equals("crédito")){
+                        banderaSaldo = true;
+                    }else if(banderaSaldo){
+                        banderaSaldo= false;
+                        info.setSaldoCreditoRevolutivo2(fkfk[0]);
+                        info.setDerechoMarca2(fkfk[1]);
+                    }else if(fkfk[0].equals("Nombre")){
+                        banderaClave=true;
+                    }else if(banderaClave){
+                        banderaClave = false;
+                        banderaNombre = true;
+                        info.setClaveCliente(fkfk[0]);
+                        nombre = fkfk[1]+" "+fkfk[2]+ " ";
+                    }else if(banderaNombre){
+                        banderaNombre = false;
+                        banderaMoneda = true;
+                        nombre += a.replace(";"," ");
+                        info.setNombre(nombre);
+                    }else if(banderaMoneda){
+                        banderaMoneda = false;
+                        info.setMoneda(fkfk[0]);
+                        info.setFechaCorte(fkfk[1]);
+                    }
+                }
+                else{
+                    System.out.println(cont +" "+a);
+                }
+                cont++;
+            }
+            System.out.println(info.toString());
+            try{
+            Excel.escribirExcel(pathExc,transaccions,info);
+            Platform.runLater(
+                    () -> {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Listo!");
+                        alert.setHeaderText("Se convirtió el archivo.");
+                        alert.setContentText("El archivo se deberá encontrar en la dirección especificada.");
+                        alert.showAndWait().ifPresent(rs -> {
+                            if (rs == ButtonType.OK) {
+                                alert.close();
+                            }
+                        });});
+            }catch(Exception e){
+                Platform.runLater(
+                        () -> {
+                            Alert alert = new Alert(Alert.AlertType.ERROR);
+                            alert.setTitle("Oops :(");
+                            alert.setHeaderText("Ocurrió un error.");
+                            alert.setContentText("El archivo no se convirtió correctamente.");
+                            alert.showAndWait().ifPresent(rs -> {
+                                if (rs == ButtonType.OK) {
+                                    alert.close();
+                                }
+                            });});
+            }
+        }catch (IOException ioe){
+            Platform.runLater(
+                    () -> {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("ERROR!");
+                        alert.setHeaderText("Ocurrió un error");
+                        alert.setContentText("Verifique el archivo PDF cargado.");
+                        alert.showAndWait().ifPresent(rs -> {
+                            if (rs == ButtonType.OK) {
+                                txtPDF.setText("");
+                                alert.close();
+                            }
+                        });});
+        }
+    }
 }
